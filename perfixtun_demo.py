ac004519cafe507a82015274f7606584a4d27fa5
huanghaian <huanghaian@sensetime.com>
2023-05-05 15:39:06 +0800

    update perfix

--- perfixtun_demo.py
+++ perfixtun_demo.py
@@ -4,16 +4,16 @@ from peft import get_peft_model, PrefixTuningConfig, TaskType
 # model_name_or_path = "bigscience/mt0-large"
 model_name_or_path = "bigscience/mt0-small"
 
-peft_config = PrefixTuningConfig(task_type=TaskType.SEQ_2_SEQ_LM, inference_mode=False, num_virtual_tokens=8)
+# 对于这个模型，token_dim 必须要设置为 384，否则由于维度不匹配， model.generate 会报错
+peft_config = PrefixTuningConfig(task_type=TaskType.SEQ_2_SEQ_LM, inference_mode=False, num_virtual_tokens=20, token_dim=384)
 
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path)
 # print(model)
 
 model = get_peft_model(model, peft_config)
-print(model)
-model.print_trainable_parameters() # trainable params: 344064 || all params: 300520832 || trainable%: 0.11448923447676333
+# print(model)
+model.print_trainable_parameters() # trainable params: 122880 || all params: 300299648 || trainable%: 0.04091912888289499
 
-# once forward--权重没有估计，也没有merge
 tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
 inputs = tokenizer.encode("Translate to English: Je t’aime.", return_tensors="pt")
 outputs = model.generate(input_ids=inputs)
